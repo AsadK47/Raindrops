0ae764c248dd3f2b682dfcbd0039b597726ffbb7
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-15 15:12:27 +0000

    Added simplest test to return 'Pling' if number is 3

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -7,5 +7,9 @@ describe Raindrops do
     it 'is a method which does not raise errors and accepts an argument' do
       expect(raindrops).to respond_to(:convert).with(1).argument
     end
+
+    it 'can return Pling' do
+      expect(raindrops.convert(3)).to eq('Pling')
+    end
   end
 end
