1fc6192fcc3c10286026c6a1874497c11b811175
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-15 19:02:52 +0000

    Reducing Rubocop offences - perhaps slightly unreadable?

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,18 +1,18 @@
 class Raindrops
   def convert(input)
-    if is_a_factor_of_3_nums?(input, 3, 5, 7)
+    if a_factor_of_3_nums?(input, 3, 5, 7)
       'PlingPlangPlong'
-    elsif is_a_factor_of_2_nums?(input, 5, 7)
+    elsif a_factor_of_2_nums?(input, 5, 7)
       'PlangPlong'
-    elsif is_a_factor_of_2_nums?(input, 3, 7)
+    elsif a_factor_of_2_nums?(input, 3, 7)
       'PlingPlong'
-    elsif is_a_factor_of_2_nums?(input, 3, 5)
+    elsif a_factor_of_2_nums?(input, 3, 5)
       'PlingPlang'
-    elsif is_a_factor_of_1_nums?(input, 3)
+    elsif a_factor_of_1_nums?(input, 3)
       'Pling'
-    elsif is_a_factor_of_1_nums?(input, 5)
+    elsif a_factor_of_1_nums?(input, 5)
       'Plang'
-    elsif is_a_factor_of_1_nums?(input, 7)
+    elsif a_factor_of_1_nums?(input, 7)
       'Plong'
     else
       input
@@ -21,15 +21,15 @@ class Raindrops
 
   private
 
-  def is_a_factor_of_1_nums?(input, a)
-    input % a == 0
+  def a_factor_of_1_nums?(input, factor1)
+    (input % factor1).zero?
   end
 
-  def is_a_factor_of_2_nums?(input, a, b)
-    input % a == 0 && input % b == 0
+  def a_factor_of_2_nums?(input, factor1, factor2)
+    (input % factor1).zero? && (input % factor2).zero?
   end
 
-  def is_a_factor_of_3_nums?(input, a, b, c)
-    input % a == 0 && input % b == 0 && input % c == 0
+  def a_factor_of_3_nums?(input, factor1, factor2, factor3)
+    (input % factor1).zero? && (input % factor2).zero? && (input % factor3).zero?
   end
 end
