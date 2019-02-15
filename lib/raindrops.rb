93f4606546836c696d9db4eb21c0f9f7aed0ff25
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-15 18:55:14 +0000

    Added private method for a factor of 2 numbers

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -2,11 +2,11 @@ class Raindrops
   def convert(n)
     if is_a_factor_of_3_nums?(n, 3, 5, 7)
       'PlingPlangPlong'
-    elsif factor_of_5_and_7?(n)
+    elsif is_a_factor_of_2_nums?(n, 5, 7)
       'PlangPlong'
-    elsif factor_of_3_and_7?(n)
+    elsif is_a_factor_of_2_nums?(n, 3, 7)
       'PlingPlong'
-    elsif factor_of_3_and_5?(n)
+    elsif is_a_factor_of_2_nums?(n, 3, 5)
       'PlingPlang'
     elsif factor_of_3?(n)
       'Pling'
@@ -33,16 +33,8 @@ class Raindrops
     n % 7 == 0
   end
 
-  def factor_of_3_and_5?(n)
-    n % 3 == 0 && n % 5 == 0
-  end
-
-  def factor_of_3_and_7?(n)
-    n % 3 == 0 && n % 7 == 0
-  end
-
-  def factor_of_5_and_7?(n)
-    n % 5 == 0 && n % 7 == 0
+  def is_a_factor_of_2_nums?(n, a, b)
+    n % a == 0 && n % b == 0
   end
 
   def is_a_factor_of_3_nums?(n, a, b, c)
