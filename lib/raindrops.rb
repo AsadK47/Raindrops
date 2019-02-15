71b28482fcfe7a04bca36fbde9e4b041397fe6bb
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-15 18:58:28 +0000

    With rubocop's help, made methods more clear

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,35 +1,35 @@
 class Raindrops
-  def convert(n)
-    if is_a_factor_of_3_nums?(n, 3, 5, 7)
+  def convert(input)
+    if is_a_factor_of_3_nums?(input, 3, 5, 7)
       'PlingPlangPlong'
-    elsif is_a_factor_of_2_nums?(n, 5, 7)
+    elsif is_a_factor_of_2_nums?(input, 5, 7)
       'PlangPlong'
-    elsif is_a_factor_of_2_nums?(n, 3, 7)
+    elsif is_a_factor_of_2_nums?(input, 3, 7)
       'PlingPlong'
-    elsif is_a_factor_of_2_nums?(n, 3, 5)
+    elsif is_a_factor_of_2_nums?(input, 3, 5)
       'PlingPlang'
-    elsif is_a_factor_of_1_nums?(n, 3)
+    elsif is_a_factor_of_1_nums?(input, 3)
       'Pling'
-    elsif is_a_factor_of_1_nums?(n, 5)
+    elsif is_a_factor_of_1_nums?(input, 5)
       'Plang'
-    elsif is_a_factor_of_1_nums?(n, 7)
+    elsif is_a_factor_of_1_nums?(input, 7)
       'Plong'
     else
-      n
+      input
     end
   end
 
   private
 
-  def is_a_factor_of_1_nums?(n, a)
-    n % a == 0
+  def is_a_factor_of_1_nums?(input, a)
+    input % a == 0
   end
 
-  def is_a_factor_of_2_nums?(n, a, b)
-    n % a == 0 && n % b == 0
+  def is_a_factor_of_2_nums?(input, a, b)
+    input % a == 0 && input % b == 0
   end
 
-  def is_a_factor_of_3_nums?(n, a, b, c)
-    n % a == 0 && n % b == 0 && n % c == 0
+  def is_a_factor_of_3_nums?(input, a, b, c)
+    input % a == 0 && input % b == 0 && input % c == 0
   end
 end
